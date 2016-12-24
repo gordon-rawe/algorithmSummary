08f57c1aaee50092ce5560f3b49bbc8b63c16ee0
gordon <gordon@Jason-Air.local>
2016-12-25 00:42:26 +0800

    update code

--- src/main/java/dicepossibility/DicePossibility.java
+++ src/main/java/dicepossibility/DicePossibility.java
@@ -38,7 +38,45 @@ public class DicePossibility {
         }
     }
 
+    public static void printPossibilities(int size) {
+        if (size < 1) return;
+        int[][] possibilities = new int[][]{new int[size * MAX_SIDES + 1], new int[size * MAX_SIDES + 1]};
+        for (int i = 0; i < size * MAX_SIDES + 1; i++) {
+            possibilities[0][i] = 0;
+            possibilities[1][i] = 0;
+        }
+        int flag = 0;
+        for (int i = 1; i <= MAX_SIDES; i++) {
+            possibilities[flag][i] = 1;
+        }
+        for (int curSize = 2; curSize <= size; curSize++) {
+            for (int i = 0; i < curSize; i++) {
+                possibilities[1 - flag][i] = 0;
+            }
+            for (int i = curSize; i <= MAX_SIDES * curSize; i++) {
+                possibilities[1 - flag][i] = 0;
+                for (int k = 1; k <= i && k <= MAX_SIDES; k++) {
+                    possibilities[1 - flag][i] += possibilities[flag][i - k];
+                    System.out.println("---");
+                    for (int j = 0; j < possibilities[flag].length; j++) {
+                        System.out.print(possibilities[flag][j] + "\t");
+                    }
+                    System.out.println();
+                    for (int j = 0; j < possibilities[1 - flag].length; j++) {
+                        System.out.print(possibilities[1 - flag][j] + "\t");
+                    }
+                    System.out.println();
+                }
+            }
+            flag = 1 - flag;
+        }
+        double total = Math.pow(MAX_SIDES, size);
+        for (int i = size; i <= size * MAX_SIDES; i++) {
+            System.out.println(i + " -> " + possibilities[flag][i] / total);
+        }
+    }
+
     public static void main(String[] args) {
-        printPossibility(6);
+        printPossibilities(3);
     }
 }
